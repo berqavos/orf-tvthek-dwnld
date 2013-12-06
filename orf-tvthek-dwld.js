c916626020fc749840bdfeaf6dc080beed4ecad4
berq <berq@users.noreply.github.com>
2013-12-06 23:30:28 +0100

    and now it's real jason

--- orf-tvthek-dwld.js
+++ orf-tvthek-dwld.js
@@ -5,7 +5,9 @@
 
 var element = document.evaluate( '/html/head/script[2]/text()' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ); 
 var almost_json = element.singleNodeValue.data.replace(/ /g,''); 
-var json = almost_json.match(/initializeAdworx\((.*?)\)/);
+var json = almost_json.match(/initializeAdworx\(\[(.*?)\]\)/);
 json[1];
 
 obj = JSON.parse(json[1]);
+
+
