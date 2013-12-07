4d29992bf881f40069662665fd6b91b5a0c2ce15
berq <berq@users.noreply.github.com>
2013-12-07 13:12:27 +0100

    converted to a greasemonkey userscript

--- orf-tvthek-dwnld.js
+++ orf-tvthek-dwnld.js
@@ -1,11 +1,34 @@
-//
-// thx to https://github.com/pxlpnk for the xpath hint
-//
+// ==UserScript==
+// @name        orf-tvthek
+// @namespace   white
+// @include    http://tvthek.orf.at/program/*
+// @version     1
+// @grant       none
+// thx to pxlpnk for the xpath hint
+// ==/UserScript==
 
 
-var element = document.evaluate( '/html/head/script[2]/text()' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ); 
-var almost_json = element.singleNodeValue.data.replace(/ /g,''); 
-var json = almost_json.match(/initializeAdworx\(\[(.*?)\]\)/);
 
-obj = JSON.parse(json[1]);
-obj.values.segment.playlist_item_array.sources[13].src;
+	var element = document.evaluate('/html/head/script[2]/text()' ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+	var almost_json = element.singleNodeValue.data.replace(/ /g,'');
+	var json = almost_json.match(/initializeAdworx\(\[(.*?)\]\)/);
+
+	obj = JSON.parse(json[1]);
+	var str = obj.values.segment.playlist_item_array.sources[13].src;
+
+var note=document.getElementById("DwnldLnk");
+if(!note){
+ note=document.createElement("div");
+ note.id="DwnldLnk";
+ document.body.appendChild(note);
+}
+
+note.innerHTML="<a href=" + str + ">click me to download video</a>";
+note.style.position="fixed";
+note.style.top="2em";
+note.style.left="0";
+note.style.background="#fff";
+note.style.border="solid 3px green";
+note.style.width="auto";
+note.style.padding="1em";
+note.style.display="block";
